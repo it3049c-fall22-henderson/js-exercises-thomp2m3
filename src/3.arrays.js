6ab661cb734823ec576dd23d7a61ba62cf65e2c3
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-08-29 22:09:58 -0400

    Update 3.arrays.js
    Finished test for filtering.

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -20,7 +20,11 @@ function sum(arr) {
 // Filter: Should filter out all instances of a value from an array
 // Example: filtering 2 out of [1,2,3,5,6,2,4,2] should return [1,3,5,6,4]
 function filterOut(arr, item) {
-  return ;
+  function removeItem(value) {
+    return value != item;
+  }
+  const filtered = arr.filter(removeItem)
+  return filtered;
 }
 
 // Append: you should be able to add an item to the end of an array
