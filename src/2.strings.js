55924becf9d20c7044e6d29ff55a296098807bc1
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-08-29 21:32:10 -0400

    Update 2.strings.js
    Passed all testing for this suite.

--- src/2.strings.js
+++ src/2.strings.js
@@ -9,19 +9,19 @@ function reverseString(str) {
    * 
    * Can even be done on one line
    */
-  return ;
+  return str.split("").reverse().join("");
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+  return str.toUpperCase();
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  return str.split(splitAt);
 }
 module.exports = {
   reverseString,
