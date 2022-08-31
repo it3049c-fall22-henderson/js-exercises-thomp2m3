322c1582c1ac3119aedaa864e2004d6caa15aac5
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-08-30 20:39:18 -0400

    Update 5.logicalOperators.js
    Finished unit test for logicalOperators

--- src/5.logicalOperators.js
+++ src/5.logicalOperators.js
@@ -2,12 +2,14 @@
 
 // or: should return true if either a or b are truthy. False, if both are false
 function or(a, b) {
-  return ;
+  x = (a || b)
+  return x;
 }
 
 //and: should return true only if both a and b are truthy
 function and(a, b) {
-  return ;
+  x = (a && b)
+  return x;
 }
 
 module.exports = {
